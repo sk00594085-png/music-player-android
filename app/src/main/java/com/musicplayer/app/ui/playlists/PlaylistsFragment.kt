7fe403fb91a2079fd9919a9994bb9556bb93029d
sk00594085-png <sk00594085@gmail.com>
2026-08-11 18:13:07 +0530

    fix: app-scoped ViewModel to stop crash - PlaylistsFragment.kt

--- app/src/main/java/com/musicplayer/app/ui/playlists/PlaylistsFragment.kt
+++ app/src/main/java/com/musicplayer/app/ui/playlists/PlaylistsFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.musicplayer.app.MusicPlayerApp
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.musicplayer.app.R
 import com.musicplayer.app.databinding.FragmentPlaylistsBinding
@@ -28,7 +28,7 @@ class PlaylistsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(requireActivity())[MusicViewModel::class.java]
+        viewModel = (requireActivity().application as MusicPlayerApp).musicViewModel
 
         val adapter = PlaylistSectionAdapter(
             onPlaylistClick = { playlist ->
