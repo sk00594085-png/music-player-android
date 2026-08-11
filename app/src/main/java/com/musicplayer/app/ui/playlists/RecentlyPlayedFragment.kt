d79a48eba026bffdcc39980c6a07e14c929df098
sk00594085-png <sk00594085@gmail.com>
2026-08-11 15:11:44 +0530

    Fix adapter signature: RecentlyPlayedFragment.kt

--- app/src/main/java/com/musicplayer/app/ui/playlists/RecentlyPlayedFragment.kt
+++ app/src/main/java/com/musicplayer/app/ui/playlists/RecentlyPlayedFragment.kt
@@ -30,9 +30,10 @@ class RecentlyPlayedFragment : BottomSheetDialogFragment() {
 
         binding.tvPlaylistTitle.text = "Recently Played"
 
-        adapter = SongAdapter { index ->
+        adapter = SongAdapter { song ->
             val list = adapter.currentList
-            if (list.isNotEmpty()) {
+            val index = list.indexOf(song)
+            if (index >= 0) {
                 viewModel.playSongsAt(list, index)
                 dismiss()
             }
