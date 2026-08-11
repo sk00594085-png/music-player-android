ec4224e01ebd58861c62531cd47950d82cf07538
sk00594085-png <sk00594085@gmail.com>
2026-08-11 15:11:46 +0530

    Fix adapter signature: MostPlayedFragment.kt

--- app/src/main/java/com/musicplayer/app/ui/playlists/MostPlayedFragment.kt
+++ app/src/main/java/com/musicplayer/app/ui/playlists/MostPlayedFragment.kt
@@ -30,9 +30,10 @@ class MostPlayedFragment : BottomSheetDialogFragment() {
 
         binding.tvPlaylistTitle.text = "Most Played"
 
-        adapter = SongAdapter { index ->
+        adapter = SongAdapter { song ->
             val list = adapter.currentList
-            if (list.isNotEmpty()) {
+            val index = list.indexOf(song)
+            if (index >= 0) {
                 viewModel.playSongsAt(list, index)
                 dismiss()
             }
