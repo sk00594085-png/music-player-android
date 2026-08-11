252e06e2cd1d396f6a541416c9544134fc79bd59
sk00594085-png <sk00594085@gmail.com>
2026-08-11 13:55:56 +0530

    Upload app/src/main/java/com/musicplayer/app/ui/playlists/RecentlyPlayedFragment.kt

---
+++ app/src/main/java/com/musicplayer/app/ui/playlists/RecentlyPlayedFragment.kt
@@ -0,0 +1,56 @@
+package com.musicplayer.app.ui.playlists
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.musicplayer.app.databinding.FragmentPlaylistDetailBinding
+import com.musicplayer.app.ui.songs.SongAdapter
+import com.musicplayer.app.viewmodel.MusicViewModel
+
+class RecentlyPlayedFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentPlaylistDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPlaylistDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewModel = ViewModelProvider(requireActivity())[MusicViewModel::class.java]
+
+        binding.tvPlaylistTitle.text = "🕐 Recently Played"
+
+        val adapter = SongAdapter { index ->
+            val list = adapter.currentList
+            if (list.isNotEmpty()) {
+                viewModel.playSongsAt(list, index)
+                dismiss()
+            }
+        }
+
+        binding.recyclerSongs.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerSongs.adapter = adapter
+
+        viewModel.recentlyPlayedSongs.observe(viewLifecycleOwner) { songs ->
+            adapter.submitList(songs)
+        }
+
+        viewModel.currentSong.observe(viewLifecycleOwner) { song ->
+            song?.let { adapter.setActiveSong(it.id) }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
