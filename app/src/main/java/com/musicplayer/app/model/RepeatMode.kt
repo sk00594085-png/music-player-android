e38c619e0587e877e8b2f93aabed7e6b17f0894e
sk00594085-png <sk00594085@gmail.com>
2026-08-11 11:49:24 +0530

    Upload app/src/main/java/com/musicplayer/app/model/RepeatMode.kt

---
+++ app/src/main/java/com/musicplayer/app/model/RepeatMode.kt
@@ -0,0 +1,7 @@
+package com.musicplayer.app.model
+
+enum class RepeatMode {
+    NONE,       // play to end, stop
+    ALL,        // loop the whole queue
+    ONE         // repeat current track
+}
