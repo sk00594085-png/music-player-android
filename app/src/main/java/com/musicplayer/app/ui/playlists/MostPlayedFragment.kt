1e8755748df4a858d9de9b998a83bd707e53a1fe
sk00594085-png <sk00594085@gmail.com>
2026-08-11 14:43:57 +0530

    Upload app/src/main/java/com/musicplayer/app/ui/playlists/MostPlayedFragment.kt

--- app/src/main/java/com/musicplayer/app/ui/playlists/MostPlayedFragment.kt
+++ app/src/main/java/com/musicplayer/app/ui/playlists/MostPlayedFragment.kt
@@ -15,6 +15,7 @@ class MostPlayedFragment : BottomSheetDialogFragment() {
 
     private var _binding: FragmentPlaylistDetailBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: SongAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -27,9 +28,9 @@ class MostPlayedFragment : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         val viewModel = ViewModelProvider(requireActivity())[MusicViewModel::class.java]
 
-        binding.tvPlaylistTitle.text = "🔥 Most Played"
+        binding.tvPlaylistTitle.text = "Most Played"
 
-        val adapter = SongAdapter { index ->
+        adapter = SongAdapter { index ->
             val list = adapter.currentList
             if (list.isNotEmpty()) {
                 viewModel.playSongsAt(list, index)
