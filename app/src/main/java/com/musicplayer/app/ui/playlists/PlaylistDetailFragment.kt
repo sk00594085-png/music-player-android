b85f6fc404131f43cb9cc514a3d843387ed0d310
sk00594085-png <sk00594085@gmail.com>
2026-08-11 13:55:48 +0530

    Upload app/src/main/java/com/musicplayer/app/ui/playlists/PlaylistDetailFragment.kt

---
+++ app/src/main/java/com/musicplayer/app/ui/playlists/PlaylistDetailFragment.kt
@@ -0,0 +1,69 @@
+package com.musicplayer.app.ui.playlists
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.musicplayer.app.databinding.FragmentPlaylistDetailBinding
+import com.musicplayer.app.ui.songs.SongAdapter
+import com.musicplayer.app.viewmodel.MusicViewModel
+
+class PlaylistDetailFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentPlaylistDetailBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        private const val ARG_ID = "playlist_id"
+        private const val ARG_NAME = "playlist_name"
+        fun newInstance(id: Long, name: String) = PlaylistDetailFragment().apply {
+            arguments = Bundle().apply {
+                putLong(ARG_ID, id)
+                putString(ARG_NAME, name)
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPlaylistDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val playlistId = arguments?.getLong(ARG_ID) ?: return
+        val playlistName = arguments?.getString(ARG_NAME) ?: ""
+        val viewModel = ViewModelProvider(requireActivity())[MusicViewModel::class.java]
+
+        binding.tvPlaylistTitle.text = playlistName
+
+        val adapter = SongAdapter { index ->
+            val list = adapter.currentList
+            if (list.isNotEmpty()) {
+                viewModel.playSongsAt(list, index)
+                dismiss()
+            }
+        }
+
+        binding.recyclerSongs.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerSongs.adapter = adapter
+
+        viewModel.getPlaylistSongs(playlistId).observe(viewLifecycleOwner) { songs ->
+            adapter.submitList(songs)
+        }
+
+        viewModel.currentSong.observe(viewLifecycleOwner) { song ->
+            song?.let { adapter.setActiveSong(it.id) }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
